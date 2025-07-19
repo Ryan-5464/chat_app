2852c8c66f5f6771fca7a4cc56874ae180bb97c4
Ryan-5464 <rsteven.tandy@outlook.com>
2025-07-19 21:21:16 +0100

    added data entities that may or may not be needed

--- data/entities/entities.go
+++ data/entities/entities.go
@@ -1,1 +1,34 @@
 package dataEntities
+
+import (
+	typ "server/types"
+	"time"
+)
+
+type Message struct {
+	Id         typ.MessageId
+	UserId     typ.UserId
+	ChatId     typ.ChatId
+	ReplyId    typ.MessageId
+	Author     string
+	Text       string
+	CreatedAt  time.Time
+	LastEditAt time.Time
+}
+
+type Chat struct {
+	Id                 typ.ChatId
+	Name               string
+	Admin              typ.UserId
+	AdminName          string
+	MemberCount        int64
+	UnreadMessageCount int64
+}
+
+type User struct {
+	Id      typ.UserId
+	Name    string
+	Email   typ.Email
+	PwdHash typ.PwdHash
+	Joined  time.Time
+}
