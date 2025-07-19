944a339b759c9c7f11ae83b98c15e065ff25705f
Ryan-5464 <rsteven.tandy@outlook.com>
2025-07-19 18:08:00 +0100

    added data models for database

--- services/dbService/models/SQL_data_models.go
+++ services/dbService/models/SQL_data_models.go
@@ -2,6 +2,7 @@ package dbServiceModels
 
 import (
 	typ "server/types"
+	"time"
 )
 
 type User struct {
@@ -9,4 +10,29 @@ type User struct {
 	Name    string
 	Email   typ.Email
 	PwdHash typ.PwdHash
+	Joined  time.Time
+}
+
+type Chat struct {
+	Id        typ.ChatId
+	Name      string
+	Admin     typ.UserId
+	CreatedAt time.Time
+}
+
+type Message struct {
+	Id         typ.MessageId
+	UserId     typ.UserId
+	ChatId     typ.ChatId
+	ReplyId    typ.MessageId
+	Text       string
+	CreatedAt  time.Time
+	LastEditAt time.Time
+}
+
+type Member struct {
+	UserId        typ.UserId
+	ChatId        typ.ChatId
+	LastReadMsgId typ.MessageId
+	Joined        time.Time
 }
--- types/types.go
+++ types/types.go
@@ -1,5 +1,17 @@
 package types
 
+type ChatId int64
+
+func (c ChatId) Int64() int64 {
+	return int64(c)
+}
+
+type MessageId int64
+
+func (m MessageId) Int64() int64 {
+	return int64(m)
+}
+
 type UserId int64
 
 func (u UserId) Int64() int64 {
