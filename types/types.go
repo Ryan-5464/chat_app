79effe1b71a4cf80c890828f57cd79a8b8489d9e
Ryan-5464 <rsteven.tandy@outlook.com>
2025-07-20 19:52:39 +0100

    added interfacing and factory for dbservices

--- types/types.go
+++ types/types.go
@@ -17,3 +17,36 @@ type UserId int64
 func (u UserId) Int64() int64 {
 	return int64(u)
 }
+
+type Rows []map[string]any
+
+type DbProvider string
+
+func (d DbProvider) String() string {
+	return string(d)
+}
+
+const (
+	SQLite3    DbProvider = "sqlite3"
+	PostgreSQL DbProvider = "postgreSQL"
+)
+
+func NewCredentials(dbProvider DbProvider, details map[string]string) Credentials {
+	return Credentials{
+		dbProvider: dbProvider,
+		details:    details,
+	}
+}
+
+type Credentials struct {
+	dbProvider DbProvider
+	details    map[string]string
+}
+
+func (c Credentials) Provider() string {
+	return string(c.dbProvider)
+}
+
+func (c Credentials) Value(key string) string {
+	return c.details[key]
+}
