6a6067876dcbadaf0aec621c93e375bd2634ddb9
Ryan-5464 <rsteven.tandy@outlook.com>
2025-07-29 14:52:14 +0100

    added redirect from index page to chat page if user is authenticated

--- handler/index.go
+++ handler/index.go
@@ -1,6 +1,8 @@
 package handler
 
 import (
+	"errors"
+	"log"
 	"net/http"
 	i "server/interfaces"
 	"text/template"
@@ -18,6 +20,38 @@ type IndexHandler struct {
 
 func (i *IndexHandler) RenderIndexPage(w http.ResponseWriter, r *http.Request) {
 	i.lgr.LogFunctionInfo()
+
+	if r.Method != http.MethodGet {
+		http.Error(w, "request method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+
+	var cookieFound bool
+	cookie, err := r.Cookie("session_token")
+	if err != nil {
+		if errors.Is(err, http.ErrNoCookie) {
+			cookieFound = true
+		} else {
+			cookieFound = false
+			http.Error(w, "Internal server error", http.StatusInternalServerError)
+			return
+		}
+	}
+
+	if cookieFound {
+		token := cookie.Value
+		session, err := l.authS.ValidateAndRefreshSession(token)
+		if err != nil {
+			log.Println("error validating or refreshing session", err)
+			http.Error(w, "Internal server error", http.StatusInternalServerError)
+			return
+		}
+
+		http.SetCookie(w, session.Cookie())
+
+		http.Redirect(w, r, "/chat", http.StatusSeeOther)
+		return
+	}
 
 	tmpl, err := template.ParseFiles("./static/pages/index.html")
 	if err != nil {
