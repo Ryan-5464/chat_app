eebd45fc6185b11f23190110bbc0faa2568eb625
Ryan-5464 <rsteven.tandy@outlook.com>
2025-07-29 14:50:23 +0100

    added redirect from register page to chat page if user is authenticated

--- handler/register.go
+++ handler/register.go
@@ -1,6 +1,7 @@
 package handler
 
 import (
+	"errors"
 	"log"
 	"net/http"
 	ent "server/data/entities"
@@ -25,6 +26,38 @@ type RegisterHandler struct {
 
 func (rh *RegisterHandler) RenderRegisterPage(w http.ResponseWriter, r *http.Request) {
 	rh.lgr.LogFunctionInfo()
+
+	if r.Method != http.MethodGet {
+		http.Error(w, "request method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+
+	var cookieFound bool
+	cookie, err := r.Cookie("session_token")
+	if err != nil {
+		if errors.Is(err, http.ErrNoCookie) {
+			cookieFound = true
+		} else {
+			cookieFound = false
+			http.Error(w, "Internal server error", http.StatusInternalServerError)
+			return
+		}
+	}
+
+	if cookieFound {
+		token := cookie.Value
+		session, err := l.authS.ValidateAndRefreshSession(token)
+		if err != nil {
+			log.Println("error validating or refreshing session", err)
+			http.Error(w, "Internal server error", http.StatusInternalServerError)
+			return
+		}
+
+		http.SetCookie(w, session.Cookie())
+
+		http.Redirect(w, r, "/chat", http.StatusSeeOther)
+		return
+	}
 
 	tmpl, err := template.ParseFiles("./static/pages/register.html")
 	if err != nil {
